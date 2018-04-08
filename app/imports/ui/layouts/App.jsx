e3bc5e2684cb84a9c2ade0c8b5adf4639b541c4b
Billy Troy Wooton <gcowootont@gmail.com>
2018-04-07 15:29:30 -1000

    Cleaned out template code and fixed NavBar; Changed NavBar color (#22)
    * Removed AddStuff and ListStuff
    
    * Changed NavBar header to 'Small Kine Jobs'
    
    * Removed ListStuff and StuffItem
    
    * Removed ListStuffAdmin and StuffItemAdmin
    
    * Removed AddStuff page
    
    * Changed navbar menu color to blue

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -7,9 +7,6 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-d
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
-import ListStuff from '../pages/ListStuff';
-import ListStuffAdmin from '../pages/ListStuffAdmin';
-import AddStuff from '../pages/AddStuff';
 import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
@@ -30,11 +27,8 @@ class App extends React.Component {
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
               <Route path="/job-search-results" component={JobSearchResult} />
-              <ProtectedRoute path="/list" component={ListStuff}/>
-              <ProtectedRoute path="/add" component={AddStuff}/>
               <ProtectedRoute path="/profile" component={DisplayProfile}/>
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
-              <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
               <ProtectedRoute path="/signout" component={Signout}/>
               <Route component={NotFound}/>
             </Switch>
