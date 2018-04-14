05489534fcf33856d4dc704c4ffdb2bc3270d3d3
Billy Troy Wooton <gcowootont@gmail.com>
2018-04-13 22:41:58 -1000

    Publish the Jobs collection

--- app/imports/startup/server/jobs.js
+++ app/imports/startup/server/jobs.js
@@ -1,34 +1,22 @@
 import { Meteor } from 'meteor/meteor';
-import { Roles } from 'meteor/alanning:roles';
-import { Stuffs } from '../../api/stuff/stuff.js';
+import { Jobs } from '../../api/jobs/jobs.js';
 
-/** Initialize the database with a default data document. */
-function addData(data) {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.insert(data);
+/** Initialize the database with a default job document. */
+function addJob(job) {
+  job.postDate = Date.parse(job.postDate); // eslint-disable-line
+  console.log(`  Adding: ${job.title} Skills: (${job.skills.map((skill) => skill.name)})`);
+  Jobs.insert(job);
 }
 
 /** Initialize the collection if empty. */
-if (Stuffs.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
+if (Jobs.find().count() === 0) {
+  if (Meteor.settings.defaultJobs) {
     console.log('Creating default data.');
-    Meteor.settings.defaultData.map(data => addData(data));
+    Meteor.settings.defaultJobs.map(job => addJob(job));
   }
 }
 
-/** This subscription publishes only the documents associated with the logged in user */
-Meteor.publish('Stuff', function publish() {
-  if (this.userId) {
-    const username = Meteor.users.findOne(this.userId).username;
-    return Stuffs.find({ owner: username });
-  }
-  return this.ready();
-});
-
-/** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
-Meteor.publish('StuffAdmin', function publish() {
-  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-    return Stuffs.find();
-  }
-  return this.ready();
+/** This subscription publishes all jobs that are currently open */
+Meteor.publish('Jobs', function publish() {
+  return Jobs.find({ open: true });
 });
