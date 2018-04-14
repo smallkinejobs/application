debb663087dc500ccdffb51d80812c8a4ab566db
Billy Troy Wooton <gcowootont@gmail.com>
2018-04-13 22:42:48 -1000

    Subscribed to Jobs collection

--- app/imports/ui/pages/JobSearchResult.jsx
+++ app/imports/ui/pages/JobSearchResult.jsx
@@ -1,76 +1,16 @@
 import React from 'react';
-import { distanceInWordsToNow, subDays } from 'date-fns';
+import { distanceInWordsToNow } from 'date-fns';
 import PropTypes from 'prop-types';
 import qs from 'query-string';
 import { Grid, Header, Card, Modal, Image, Button, Label, Input, Loader } from 'semantic-ui-react';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
 import JobCard from '../components/JobCard';
+import { Jobs } from '../../api/jobs/jobs.js';
 
 /** Renders the Page for job search results. */
 class JobSearchResult extends React.Component {
 
-  jobs = [
-    {
-      _id: 1,
-      title: 'Job 1',
-      description: 'Leverage agile frameworks to provide a robust synopsis for high level overviews.',
-      location: 'Campus Center',
-      pay: 12.44,
-      postDate: subDays(new Date(), 3),
-      skills: [
-        { _id: 1, name: 'technology' },
-        { _id: 2, name: 'IT' },
-        { _id: 3, name: 'programming' },
-      ],
-    },
-    {
-      _id: 2,
-      title: 'Job 11',
-      description: 'Bring to the table win-win survival strategies to ensure proactive domination.',
-      location: 'Food Court',
-      pay: 11.22,
-      postDate: subDays(new Date(), 1),
-      skills: [
-        { _id: 1, name: 'MS Word' },
-        { _id: 2, name: 'IT' },
-        { _id: 3, name: 'Typing' },
-      ],
-    },
-    {
-      _id: 3,
-      title: 'Job 123',
-      description: 'Capitalise on low hanging fruit to identify a ballpark value added activity to beta test.',
-      location: 'Library',
-      pay: 10.11,
-      postDate: subDays(new Date(), 10),
-      skills: [
-        { _id: 1, name: 'AWS' },
-        { _id: 2, name: 'Unix' },
-      ],
-    },
-    {
-      _id: 4,
-      title: 'Job 12',
-      description: 'Capitalise on low hanging fruit to identify a ballpark value added activity to beta test.',
-      location: 'Landscaping',
-      pay: 12.22,
-      postDate: subDays(new Date(), 3),
-      skills: [
-        { _id: 2, name: 'React' },
-        { _id: 3, name: 'UI Design' },
-      ],
-    },
-    {
-      _id: 5,
-      title: 'Job 135',
-      description: 'Capitalise on low hanging fruit to identify a ballpark value added activity to beta test.',
-      location: 'Athletic Center',
-      pay: 10.77,
-      postDate: subDays(new Date(), 22),
-      skills: [
-        { _id: 3, name: 'Project Management' },
-      ],
-    },
-  ];
   /** Bind 'this' so that a ref to the Form can be saved in formRef and communicated between render() and submit(). */
   constructor(props) {
     super(props);
@@ -78,7 +18,7 @@ class JobSearchResult extends React.Component {
     this.state = {
       loading: false,
       jobSearchText: '',
-      jobs: this.jobs,
+      jobs: [],
       modalOpen: false,
       selectedJob: {
         skills: [],
@@ -92,18 +32,24 @@ class JobSearchResult extends React.Component {
   componentDidMount() {
     const { location } = this.props;
     const queryParams = qs.parse(location.search);
-    const jobs = this.jobs.filter((job) => job.title.includes(queryParams.title));
     this.setState({
-      jobs,
       jobSearchText: queryParams.title,
     });
     this.filterJobResults = this.filterJobResults.bind(this);
   }
 
+  getJobs() {
+    const jobs = this.props.jobs;
+    const filteredJobs = jobs.filter((job) => job.title.includes(this.state.jobSearchText));
+    this.setState({
+      jobs: filteredJobs,
+    });
+  }
+
   openModal(id) {
     this.setState({
       modalOpen: true,
-      selectedJob: this.jobs.find((job) => job._id === id),
+      selectedJob: this.state.jobs.find((job) => job._id === id),
     });
   }
 
@@ -125,7 +71,7 @@ class JobSearchResult extends React.Component {
     this.setState({
       loading: true,
     });
-    const jobs = this.jobs.filter((job) => job.title.includes(data.value));
+    const jobs = Jobs.find({}).fetch().filter((job) => job.title.includes(data.value));
     setTimeout(() => {
       this.setState({
         jobSearchText: data.value,
@@ -135,8 +81,14 @@ class JobSearchResult extends React.Component {
     }, 100);
   }
 
-  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   render() {
+    return (this.props.ready) ? this.renderPage() : <Loader>Retrieving Jobs</Loader>;
+  }
+  /** Render the job search results */
+  renderPage() {
+    if (this.state.jobs.length === 0) {
+      this.getJobs();
+    }
     const { jobs, modalOpen, selectedJob, jobSearchText, loading } = this.state;
     return (
       <div>
@@ -190,6 +142,14 @@ class JobSearchResult extends React.Component {
 
 JobSearchResult.propTypes = {
   location: PropTypes.any,
+  jobs: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
 };
 
-export default JobSearchResult;
+export default withTracker(() => {
+  const subscription = Meteor.subscribe('Jobs');
+  return {
+    ready: subscription.ready(),
+    jobs: Jobs.find({}).fetch(),
+  };
+})(JobSearchResult);
