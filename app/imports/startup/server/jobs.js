8d4faf3a47bf2354f9f2ff797d976d46930d6a51
Billy Troy Wooton <gcowootont@gmail.com>
2018-04-14 11:51:11 -1000

    Implementation of the Jobs Mongo Collection (#40)
    * Defined Jobs collection and Schema
    
    * Import the Jobs collection
    
    * Added defaultJobs
    
    * Publish the Jobs collection
    
    * Subscribed to Jobs collection
    
    * Added componentWillReceiveProps to handle async data fetch

---
+++ app/imports/startup/server/jobs.js
@@ -0,0 +1,22 @@
+import { Meteor } from 'meteor/meteor';
+import { Jobs } from '../../api/jobs/jobs.js';
+
+/** Initialize the database with a default job document. */
+function addJob(job) {
+  job.postDate = Date.parse(job.postDate); // eslint-disable-line
+  console.log(`  Adding: ${job.title} Skills: (${job.skills.map((skill) => skill.name)})`);
+  Jobs.insert(job);
+}
+
+/** Initialize the collection if empty. */
+if (Jobs.find().count() === 0) {
+  if (Meteor.settings.defaultJobs) {
+    console.log('Creating default data.');
+    Meteor.settings.defaultJobs.map(job => addJob(job));
+  }
+}
+
+/** This subscription publishes all jobs that are currently open */
+Meteor.publish('Jobs', function publish() {
+  return Jobs.find({ open: true });
+});
