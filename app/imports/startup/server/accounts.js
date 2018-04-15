2e6473590cf349fc8daa3c0c27a32e4e7b3b12b5
Billy Troy Wooton <gcowootont@gmail.com>
2018-04-14 17:38:20 -1000

    Fleshed out Meteor.users collection

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -4,15 +4,19 @@ import { Roles } from 'meteor/alanning:roles';
 
 /* eslint-disable no-console */
 
-function createUser(email, password, role) {
+function createUser(email, password, profile, role) {
   console.log(`  Creating user ${email}.`);
   const userID = Accounts.createUser({
     username: email,
     email: email,
     password: password,
+    profile: profile,
   });
-  if (role === 'admin') {
-    Roles.addUsersToRoles(userID, 'admin');
+  if (role === 'employee') {
+    Roles.addUsersToRoles(userID, 'employee');
+  }
+  if (role === 'employer') {
+    Roles.addUsersToRoles(userID, 'employer');
   }
 }
 
@@ -20,7 +24,8 @@ function createUser(email, password, role) {
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, role }) => createUser(email, password, role));
+    const defaultAccounts = Meteor.settings.defaultAccounts;
+    defaultAccounts.map(({ email, password, profile, role }) => createUser(email, password, profile, role));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
